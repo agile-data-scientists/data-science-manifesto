e9e0d8743b6c2d43e6d25227d053f0f967c3c4de
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-10-25 18:06:25 +0000

    update app key

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'im6bHF7VRJIBffdOusLp6zEMGZQp71bF'),
+    'key' => env('APP_KEY', 'sB0C9KwJsSbubN8bi2sBFBZwKDlF0Wfq'),
 
     'cipher' => 'AES-256-CBC',
 
