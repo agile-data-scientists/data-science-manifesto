200234052d45a05973c41d5b7ea4f8991ebbe43b
Gianmario <gm.spacagna@gmail.com>
2016-04-07 11:42:41 +0100

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,6 +4,5 @@
     <ul>
         <li{!! $bodyClass == 'welcome' ? ' class="active"' : '' !!}><a href="/">Home</a></li>
         <li{!! $bodyClass == 'about' ? ' class="active"' : '' !!}><a href="/about">About</a></li>
-        <li{!! $bodyClass == 'principles' ? ' class="active"' : '' !!}><a href="/principles">Principles</a></li>
     </ul>
 </nav>
